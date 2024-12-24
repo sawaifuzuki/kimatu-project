c81b5dcf2e799c64eccd299f9c6bf4c0ecf674c8
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-24 12:33:58 +0900

    Update ex1-3.js

--- ex1-3.js
+++ ex1-3.js
@@ -1,1 +1,3 @@
-
+function toi(){if(document.getElementById('answer').value=='2'){alert('あたり！');}
+                  else if(document.getElementById('answer').value=='1'){alert('惜しい！No.2');}
+                  else{alert('はずれ！');}}
