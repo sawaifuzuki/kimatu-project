338a107f25f6f46e7052569d37e627b8ffe106fe
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-24 12:36:06 +0900

    Create ex1-4.js

---
+++ ex1-4.js
@@ -0,0 +1,3 @@
+function toi2(){if(document.getElementById('answer2').value=='3'){alert('あたり！');}
+                  else if(document.getElementById('answer2').value=='4'){alert('惜しい！No.2');}
+                  else{alert('はずれ！');}}
