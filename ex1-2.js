8d4479a0cc9c4c2c2cb8aa3872bcd7c045152134
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-24 12:08:00 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,2 +1,2 @@
-function imgechange2(){document.getElementById('logo2').src='./image/vanitoy2.jpg';}
+function imgchange2(){document.getElementById('logo2').src='./image/vanitoy2.jpg';}
 function mes2(){document.getElementById('logo2').src='./image/vanitoy.jpg';}
