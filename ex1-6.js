244378b3f9cf7394e8ecf6f97516c91bc42411a5
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2025-01-16 11:36:06 +0900

    Update ex1-6.js

--- ex1-6.js
+++ ex1-6.js
@@ -1,2 +1,2 @@
-function imgchange12(){document.getElementById('logo12').src='./image/cpb2.jpg';}
-function mes12(){document.getElementById('logo12').src='./image/cpb.jpg';}
+function imgchange12(){document.getElementById('logo12').src='./image/cpb2.jpeg';}
+function mes12(){document.getElementById('logo12').src='./image/cpb.jpeg';}
